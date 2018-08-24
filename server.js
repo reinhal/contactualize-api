d56cb72057c05be0d2a3eced8061685fd9ae92e3
Lisa Reinhardt <reinal@gmail.com>
2018-08-24 14:13:27 -0500

    added CORS to the server.js file

--- server.js
+++ server.js
@@ -2,12 +2,19 @@
 
 const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+const PORT = process.env.PORT || 8080;
 
 app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
