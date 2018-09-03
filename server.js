a2f3df5ba7d866e28f5cbfbe6551f87558cb9459
Lisa Reinhardt <reinal@gmail.com>
2018-09-02 19:50:18 -0500

    added POST PUT DELETE endpoints for api/interactions and api/contacts

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
-
+ 
 app.get('/api/auth', (req, res) => {
   res.json({ ok: true });
 });
@@ -29,19 +29,60 @@ app.get('/api/interactions', (req, res) => {
     });
 });
 
-app.post('/api/interactions', jsonParser, (req, res) => {
-  const requiredFields = ['person_id', 'title', 'text'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
+app.post('/api/interactions', jsonParser, (req, res, next) => {
+  const { person_id, title, text } = req.body;
+
+  if (!person_id, !title, !text) {
+    const err = new Error(`Missing something`);
+    err.status = 400;
+    return next(err);
   }
 
-  const interaction = Interaction.create(req.body.person_id, req.body.title, req.body.text);
-  res.status(201).json(interaction);
+  Interaction.create({ person_id, title, text })
+    .then(newInteraction => {
+      res.status(201)
+        .json(newInteraction);
+    })
+    .catch(next);
+});
+
+app.put('/api/interactions/:id', jsonParser, (req, res, next) => {
+  const id = req.params.id;
+  const updatedInteraction = {};
+  const updatedFields = [ 'person_id', 'title', 'text'];
+  console.log('request = ' + req.body);
+  updatedFields.forEach( field => {
+    if (field in req.body) {
+      updatedInteraction[field] = req.body[field];
+    }
+  });
+  if (!updatedInteraction.person_id, !updatedFields.title, !updatedInteraction.text) {
+    const err = new Error ('Missing some information');
+    err.status = 400;
+    return next(err);
+  }
+  Interaction.findByIdAndUpdate(id, updatedInteraction, {new: true})
+    .then(interaction => {
+      if (interaction) {
+        res.json(interaction);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
+
+app.delete('/api/interactions/:id', (req, res, next) => {
+  const id = req.params.id;
+  Interaction.findByIdAndRemove(id)
+    .then(count => {
+      if (count) {
+        res.status(204).end();
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 });
 
 app.get('/api/contacts', (req, res) => {
@@ -51,19 +92,59 @@ app.get('/api/contacts', (req, res) => {
     });
 });
 
-app.post('/api/contacts', jsonParser, (req, res) => {
-  const requiredFields = ['interactions', 'person', 'notes'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
+app.post('/api/contacts', jsonParser, (req, res, next) => {
+  const { interactions, person, notes } = req.body;
+
+  if (!interactions, !person, !notes) {
+    const err = new Error(`Missing something`);
+    err.status = 400;
+    return next(err);
   }
 
-  const contact = Contact.create(req.body.interactions, req.body.person, req.body.notes);
-  res.status(201).json(contact);
+  Contact.create({ interactions, person, notes })
+    .then(newContact => {
+      res.status(201)
+        .json(newContact);
+    })
+    .catch(next);
+});
+
+app.put('/api/contacts/:id', jsonParser, (req, res, next) => {
+  const id = req.params.id;
+  const updatedContact = {};
+  const updatedFields = [ 'interactions', 'person', 'notes'];
+  updatedFields.forEach( field => {
+    if (field in req.body) {
+      updatedContact[field] = req.body[field];
+    }
+  });
+  if (!updatedContact.interactions, !updatedFields.person, !updatedContact.notes) {
+    const err = new Error ('Missing some information');
+    err.status = 400;
+    return next(err);
+  }
+  Contact.findByIdAndUpdate(id, updatedContact, {new: true})
+    .then(contact => {
+      if (contact) {
+        res.json(contact);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
+
+app.delete('/api/contacts/:id', (req, res, next) => {
+  const id = req.params.id;
+  Contact.findByIdAndRemove(id)
+    .then(count => {
+      if (count) {
+        res.status(204).end();
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 });
 
 mongoose.connect(MONGODB_URI)
