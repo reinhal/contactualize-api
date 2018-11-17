4e99d08ba29172196e3a2b94289f553b34ccc165
Lisa Reinhardt <reinal@gmail.com>
2018-11-17 17:32:57 -0600

    connected to mLab

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 'use strict';
-
+require('dotenv').config();
 exports.CLIENT_ORIGIN = 'http://localhost:3000';
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/contactualize';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://testUser:thinkful925@ds123361.mlab.com:23361/contactualize';
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/contactualize-test';
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
