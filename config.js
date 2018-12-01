dd254f8e1cc0f28508210074c253a64284eadd9d
Lisa Reinhardt <reinal@gmail.com>
2018-12-01 13:11:51 -0600

    updated config settings

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 'use strict';
 require('dotenv').config();
-exports.CLIENT_ORIGIN = 'http://localhost:3000';
+exports.CLIENT_ORIGIN = 'https://contactualize-client.herokuapp.com/' ||'http://localhost:3000';
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://testUser:thinkful925@ds123361.mlab.com:23361/contactualize';
-// dont' expose the actual server, move this over to Heroku
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/contactualize-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
